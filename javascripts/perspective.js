534956ee50e1656d99167ef94567e0f99a4466e9
lllafemme <onnegranditpas@gmail.com>
2018-10-10 19:15:05 +0300

    CHERDAK

---
+++ javascripts/perspective.js
@@ -0,0 +1,17 @@
+// documet.addEventListener("mousemove", function (event) {
+//
+//   const x = event.pageX
+//   const y = event.pageY
+//
+//   const taregt = document.querySelector(".card")
+//   const targetCoords = target.getBoundingClientRect()
+//
+//   const targetX = targetCoords.left + (target.offsetWidth / 2)
+//   const targetY = targetCoords.top + (target.offsetHeight / 2)
+//
+//   const angleX = (taregtY - y) / 25
+//   const angleY = (targetX - x) / 25
+//
+//   target.style.transform = "rotateX(" + angleX + "deg) rotateY(" + angleY + "deg)"
+//
+// })
